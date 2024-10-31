b9cc3e79cbff9058e152bde13e8c50627de9b922
mohitgaur310 <mohitgaur310@gmail.com>
2024-10-31 22:43:12 +0530

    refector and added edge cases

--- DSA/secondLargest.js
+++ DSA/secondLargest.js
@@ -1,19 +1,29 @@
 // find the second largest //
 
-let array = [4, 9, 0, 2, 8, 7, 1];
+let array = [4, 9, , 9, 0, 2, 8, 7, 1];
 
-let largestNo = -Infinity;
-let secondLargest = -Infinity;
+const findSecondLargest = (array) => {
+  let largestNo = -Infinity;
+  let secondLargest = -Infinity;
 
-for (let arr of array) {
-  if (largestNo < arr) {
-    secondLargest = largestNo;
-    largestNo = arr;
-  } else {
-    if (secondLargest < arr) {
+  if (array.length == 0) {
+    return "Array can not be empty!";
+  }
+
+  if (array.length < 2) {
+    return "Array should have more than or equal to 2 numbers!";
+  }
+
+  for (let arr of array) {
+    if (largestNo < arr) {
+      secondLargest = largestNo;
+      largestNo = arr;
+    } else if (secondLargest < arr && largestNo != arr) {
       secondLargest = arr;
     }
   }
-}
-console.log("🚀 ~ largestNo:", largestNo);
-console.log("🚀 ~ secondLargest:", secondLargest);
+  return secondLargest;
+};
+
+let second = findSecondLargest(array);
+console.log("🚀 ~ second:", second);
