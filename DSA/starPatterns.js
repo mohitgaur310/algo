894c6676221a1d214aa30df70e86c04c94584741
mohitgaur310 <mohitgaur310@gmail.com>
2024-11-06 23:15:44 +0530

    chagnes

--- DSA/starPatterns.js
+++ DSA/starPatterns.js
@@ -27,6 +27,8 @@ const rightStar = () => {
 // rightStar();
 
 const countNumbers = (num) => {
+  if (num == 0) return 1;
+  if (num < 0) num = Math.abs(num);
   let n = 0;
   while (num > 0) {
     num = parseInt(num / 10);
@@ -35,4 +37,35 @@ const countNumbers = (num) => {
   return n;
 };
 
-let digit = countNumbers(323123213123);
+// let digit = countNumbers(-12323);
+// console.log("🚀 ~ digit:", digit);
+
+const palindrome = (num) => {
+  if (num < 0) return false;
+  let revNum = "";
+  let num1 = num;
+  while (num1 !== 0) {
+    let n = num1 % 10;
+    num1 = Math.floor(num1 / 10);
+    revNum += n;
+  }
+  if (num == revNum) return true;
+  return false;
+};
+
+// let isPalindrome = palindrome(12321);
+// console.log("🚀 ~ isPalindrome:", isPalindrome);
+
+const reverseInteger = (num) => {
+  let limit = 2 ** 31;
+  let numCopy = num;
+  let revNum = "";
+  while (num > 0) {
+    let n = num % 10;
+    num = Math.floor(num / 10);
+    revNum += n;
+  }
+  if (revNum < -limit || revNum > limit) return 0;
+
+  return numCopy < 0 ? -revNum : revNum;
+};
