4903cd4fbf4277ddf07404f703804dbc649a4b44
mohitgaur310 <mohitgaur310@gmail.com>
2024-11-13 00:11:33 +0530

    changes for removing

--- DSA/removeDuplicates.js
+++ DSA/removeDuplicates.js
@@ -13,4 +13,24 @@ const duplicatesRemove = (nums) => {
 let nums = [1, 1, 2, 2, 2, 2, 3, 4, 4, 5, 6, 6];
 // 2
 
-duplicatesRemove(nums);
+// duplicatesRemove(nums);
+
+const removeElements = (arr, val) => {
+  let x = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != val) {
+      if (arr[i] != val) {
+        arr[x] = arr[i];
+        x++;
+      }
+    }
+  }
+  console.log("🚀 ~ removeElements ~ x:", x);
+  console.log("🚀 ~ removeElements ~ arr:", arr);
+  return x;
+};
+
+let arr = [0, 1, 2, 2, 3, 0, 4, 2];
+let val = 2;
+
+removeElements(arr, val);
