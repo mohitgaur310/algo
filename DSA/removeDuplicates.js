41bd2911ed680c763cc4334c1e296424e4f0d98d
Mohit kumar Gaur <115886566+mohitgaur310@users.noreply.github.com>
2025-08-14 11:42:44 +0530

    remove the chagnes

--- DSA/removeDuplicates.js
+++ DSA/removeDuplicates.js
@@ -34,3 +34,20 @@ let arr = [0, 1, 2, 2, 3, 0, 4, 2];
 let val = 2;
 
 removeElements(arr, val);
+// 
+
+function removeDuplicates(arr) {
+  // your code here
+  if (arr.length == 0) {
+    return []
+  }
+let newArr=[]
+  for (let i = 0; i < arr.length; i++){
+    if (!newArr.includes(arr[i])) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
+}
+removeDuplicates([1, 2, 2, 3, 4, 4])
+
